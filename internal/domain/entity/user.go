acaa6c75b5f983da31e990c7f8ba5203b68ef9b7
soicchi <soicchi@icloud.com>
2023-10-22 22:50:32 +0900

    Created user model

---
+++ internal/domain/entity/user.go
@@ -0,0 +1,12 @@
+package entity
+
+import (
+	"gorm.io/gorm"
+)
+
+type User struct {
+	gorm.Model
+	Name     string `gorm:"not null; size:255"`
+	Email    string `gorm:"not null; size:255; unique"`
+	Password string `gorm:"not null; size:255"`
+}
